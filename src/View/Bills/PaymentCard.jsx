46e39740998bccae1c6293722da58773a4de5c14
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-01-29 12:28:55 +0530

    up

--- src/View/Bills/PaymentCard.jsx
+++ src/View/Bills/PaymentCard.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import payment_card_edit_img from '../../assets/download (8).png'
+import visa_img from '../../assets/download (9).png'
 
 const PaymentCard = () => {
+  const [cardNumber, setCardNumber] = useState("7812 2139 0823 XXXX"); // Default masked card number
+  const [isEditable, setIsEditable] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditable(true);
+  };
   return (
     <>
       <div className="payment_card_wrapper">
@@ -11,9 +18,18 @@ const PaymentCard = () => {
         </div>
         <div className="card_number_edit_wrapper">
           <div className="payment_card_edit_section">
-            <img src={payment_card_edit_img} alt="" />
-            <input type="text" />
-            <i class="fa-solid fa-pen"></i>
+          <div style={{display:"flex", columnGap:"20px", alignItems:"center"}}>
+          <img src={payment_card_edit_img} alt="" style={{width:"22px",height:"16px"}}/>
+          <input type="text" value={cardNumber} onChange={(e)=>setCardNumber(e.target.value)} disabled={!isEditable}/>
+          </div>
+            <i class="fa-solid fa-pen" style={{color:"#FFF", cursor: "pointer", fontSize:"15px"}} onClick={handleEditClick}></i>
+          </div>
+          <div className="visa_card_edit_section">
+            <div style={{display:"flex", columnGap:"20px", alignItems:"center"}}>
+              <img src={visa_img} alt="" />
+              <input type="text" value={cardNumber} onChange={(e)=>setCardNumber(e.target.value)} disabled={!isEditable}/>
+            </div>
+            <i class="fa-solid fa-pen" style={{color:"#FFF", cursor: "pointer", fontSize:"15px"}} onClick={handleEditClick}></i>
           </div>
         </div>
       </div>
